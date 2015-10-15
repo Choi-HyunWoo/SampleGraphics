4e00f3ad1528ba9cc4e94f749c60a5eabbdd76e4
= <cho7673@gmail.com>
2015-10-15 11:37:41 +0900

    Initial commit

---
+++ app/src/main/java/com/hw/corcow/samplegraphics/MyView.java
@@ -0,0 +1,19 @@
+package com.hw.corcow.samplegraphics;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.view.View;
+
+/**
+ * Created by Tacademy on 2015-10-15.
+ */
+public class MyView extends View {
+    public MyView (Context context) {
+        super.(context);
+        init();
+    }
+
+    Paint nPaint = new paint();
+
+}
+
